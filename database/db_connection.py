07149ddde3658c4080ce123c6f0c4be23eeb582f
azizadx <craft@azizadx.me>
2023-12-14 11:14:20 +0000

    feat:script to connect to db is added

--- database/db_connection.py
+++ database/db_connection.py
@@ -1,1 +1,20 @@
 # move the code in juypter note book to here
+import os
+from sqlalchemy import create_engine
+
+
+class DBConnect:
+    @staticmethod
+    def connect_to_db():
+        # Fetch the database credentials from environment variables
+        db_user = os.getenv("POSTGRES_USER")
+        db_password = os.getenv("POSTGRES_PASSWORD")
+        db_port = '5435'
+        db_name = os.getenv("POSTGRES_DB")
+
+        # Create a connection to the PostgreSQL database
+        engine = create_engine(
+            f'postgresql://{db_user}:{db_password}@localhost:{db_port}/{db_name}')
+
+        # Return the fetched rows
+        return engine
