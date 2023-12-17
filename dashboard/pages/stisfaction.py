f350eea6a12c983639dbf1b4de6c101e51180a65
azizadx <azizadx.tech@gmail.com>
2023-12-17 21:30:01 +0300

    feat: refactor the dashboard code

---
+++ dashboard/pages/stisfaction.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pandas as pd
+import os
+import sys
+
+
+def app():
+
+    st.title("User Satisfaction Analysis")
+
+    st.header("User Clustering based on both scores")
+    st.image('../data/score_cluster.png')
+    st.markdown(
+        'We can boost customer happiness by increasing the **experience score**.')
